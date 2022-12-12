d66cd6916fbf39c367246f95e78bcc16ab8958f3
pangyu <1510397456@qq.com>
2022-12-12 15:05:42 +0800

    add posts

--- blueprints/employer.py
+++ blueprints/employer.py
@@ -3,7 +3,7 @@ from flask_restful import Api, Resource, reqparse, fields, marshal_with
 
 import datetime
 
-from models import Employer
+from models import Employer, Post
 from exts import db
 
 from utils import verifyToken, generateToken, verifyEmployerToken, emailByTokenStr
@@ -76,5 +76,33 @@ class BasicInfo(Resource):
             {'email': email, 'name': name, 'CEO': CEO, 'researchDirection': researchDirection, 'dateOfEstablishment': dateOfEstablishment, 'location': location, 'staff': staff}})
 
 
+class Posts(Resource):
+    @verifyEmployerToken
+    def post(self):
+        tokenStr = request.headers.get('Authorization')[9:]
+        email = emailByTokenStr(tokenStr)
+        employer = Employer.query.filter_by(email=email).first()
+        # add a new post
+        title = request.json.get('title')
+        salary = request.json.get('salary')
+        degree = request.json.get('degree')
+        label = request.json.get('label')
+        tasks = request.json.get('tasks')
+        requirements = request.json.get('requirements')
+        inRecruitment = request.json.get('inRecruitment')
+        if inRecruitment == 'true':
+            inRecruitment = True
+        else:
+            inRecruitment = False
+        post = Post(title=title, salary=salary, degree=degree, label=label, tasks=tasks, requirements=requirements, inRecruitment=inRecruitment, employerId=employer.uid)
+        try:
+            db.session.add(post)
+            db.session.commit()
+            return jsonify({'status': 200, 'msg': 'Post added successfully!'})
+        except Exception as e:
+            return jsonify({'status': 403, 'msg': str(e)})
+
+
 api.add_resource(EmployerList, '/list')
 api.add_resource(BasicInfo, '/basic-info')
+api.add_resource(Posts, '/posts')
